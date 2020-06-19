9123d114f3fec9fc615a15b13bf06c0222e24f38
rbaeschlin <64172047+rbaeschlin@users.noreply.github.com>
2020-06-19 16:19:44 -0700

    added alt text and js file to create search function

---
+++ photo_gallery_v4/js/app.js
@@ -0,0 +1,5 @@
+// Search to filter to certain images
+// return search value as lower case
+// compare search to data-title or data-alt for matches
+// display images matching search term
+// else display message no images found
